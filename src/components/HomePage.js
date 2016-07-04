dc773e479279b5dad947480314e6761e86acc0b5
tsupol <ton.supol@gmail.com>
2016-07-04 22:20:54 +0700

    test fork

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,10 +6,13 @@ import {List, ListItem} from 'material-ui/List';
 import {ContentInbox, ActionGrade, ContentSend, ContentDrafts, ActionInfo} from 'material-ui/svg-icons';
 import Divider from 'material-ui/Divider';
 
+// import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 // Forms
 import SemiText from './forms/SemiText';
 import SemiForm from './forms/SemiForm';
 import FormsyDate from 'formsy-material-ui/lib/FormsyDate';
+import FormsySelect from 'formsy-material-ui/lib/FormsySelect';
 
 import ErrorMessage from './forms/ErrorMessage';
 
@@ -17,8 +20,14 @@ class HomePage extends Component {
     constructor(props, context){
         super(props, context);
         this.state = {
-            canSubmit: false
+            value: 1
         };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event, index, value) {
+        this.setState({value});
+        console.log('this.getState()', this.state);
     }
 
     render(){
@@ -29,6 +38,7 @@ class HomePage extends Component {
                         <Panel title="Home">
                             <div className="con-pad">
                                 This is panel widget. Title is optional.
+                                Test pull request.
                             </div>
                         </Panel>
                         <Row>
@@ -55,6 +65,13 @@ class HomePage extends Component {
                                                 floatingLabelText="Password"
                                                 underlineShow={false}
                                             />
+                                            <FormsySelect name="select" value={this.state.value} onChange={this.handleChange}>
+                                                <MenuItem value={1} primaryText="Never" />
+                                                <MenuItem value={2} primaryText="Every Night" />
+                                                <MenuItem value={3} primaryText="Weeknights" />
+                                                <MenuItem value={4} primaryText="Weekends" />
+                                                <MenuItem value={5} primaryText="Weekly" />
+                                            </FormsySelect>
                                             <Divider />
                                         </SemiForm>
                                     </div>
